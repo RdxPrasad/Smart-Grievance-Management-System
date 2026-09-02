3ebf06288472a04d197b4da479842f4e8b117dfb
RdxPrasad <prasadawareofficial@gmail.com>
2026-09-03 01:32:19 +0530

    Add grievance and grievance update models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, BigInteger, Text
+from sqlalchemy import Column, Integer, String, DateTime, BigInteger, Text, ForeignKey
 from sqlalchemy.sql import func
 
 from app.database import Base
@@ -25,4 +25,25 @@ class Category(Base):
     updated_at = Column(DateTime(timezone=True),  server_default=func.now())
 
 
+class Grievance(Base):
+    __tablename__ = "grievances"
 
+    id = Column(BigInteger , primary_key=True, index=True)
+    submitted_by =  Column(BigInteger, ForeignKey('users.id'), nullable=False)
+    complaint = Column(Text, nullable=False)
+    priority = Column(String, nullable=False)
+    status = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now())
+    category_id = Column(BigInteger, ForeignKey("categories.id"), nullable=False)
+
+
+class GrievanceUpdate(Base):
+    __tablename__ = "grievance_updates"
+
+    id = Column(BigInteger , primary_key=True, index=True)
+    grievance_id = Column(BigInteger, ForeignKey("grievances.id"), nullable=False)
+    updated_by = Column(BigInteger, ForeignKey("users.id"), nullable=False) 
+    status = Column(String, nullable=False)
+    note = Column(Text, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
