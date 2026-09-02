c77e908065ad140cb6f5a51d726617828454d773
RdxPrasad <prasadawareofficial@gmail.com>
2026-09-03 01:54:50 +0530

    Add grievance and grievance update schemas

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -9,6 +9,11 @@ class UserCreate(BaseModel):
     email : str
     role : str
 
+class UserUpdate(BaseModel):
+    name : str 
+    email : str 
+    role : str
+
 class UserResponse(BaseModel):
     id : int
     name : str 
@@ -20,10 +25,6 @@ class UserResponse(BaseModel):
     class Config :
             from_attributes = True
 
-class UserUpdate(BaseModel):
-    name : str 
-    email : str 
-    role : str
 
     
 
@@ -46,4 +47,60 @@ class CategoryResponse(BaseModel):
 
     class Config :
         from_attributes = True
+
+
+
+
+# GRIEVANCES TABLE
+class GrievanceCreate(BaseModel):
+    submitted_by : int
+    complaint : str 
+    priority : str
+    status : str
+    category_id : int
+
+class GrievanceUpdate(BaseModel):
+    complaint : str 
+    priority : str 
+    status : str 
+    category_id : int
+
+class GrievanceResponse(BaseModel):
+    id : int
+    submitted_by : int
+    complaint : str
+    priority : str
+    status : str
+    category_id : int
+    created_at : datetime
+    updated_at : datetime
+
+    class Config :
+        from_attributes = True
+
+
+
+
+# GRIEVANCE_UPDATE TABLE
+class GrievanceUpdateCreate(BaseModel):
+    grievance_id : int
+    updated_by : int
+    status : str
+    note : Optional[str] = None
+
+class GrievanceUpdateUpdate(BaseModel):
+    status : str
+    note : Optional[str] = None
+
+class GrievanceUpdateResponse(BaseModel):
+    id : int 
+    grievance_id : int
+    updated_by : int
+    status : str
+    note : Optional[str] = None
+    created_at : datetime
+
+    class Config :
+        from_attributes = True
+
 
